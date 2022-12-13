f4fbb2979d4970797c9d7e126a72f024a902c97b
Jardo Rook <jardorook@localhost.localdomain>
2022-12-13 15:58:59 -0600

    finished pt2 of day 1

--- 2022/2022_01_02_calories.py
+++ 2022/2022_01_02_calories.py
@@ -3,16 +3,15 @@ f = open("2022_01_input.txt", "r")
 my_string = f.read()
 my_arr = my_string.split('\n')
 
-total = 0
+each_elf   = []
 curr_total = 0
 for num in my_arr:
     if num != '':
         curr_total = curr_total + int(num)
         #print(f"This is current number: {num}")
     else:
-        if curr_total > total:
-            total = curr_total
-            #print(f"New Total Largest Calories: {total}")
+        each_elf.append(curr_total)
         curr_total = 0
 
-print(total)
+each_elf.sort(reverse=True)
+print(each_elf[0]+ each_elf[1] + each_elf[2])
