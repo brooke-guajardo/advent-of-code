f0308677894de3dcfbdca2955b0998ad4a452238
Jardo Rook <jardorook@localhost.localdomain>
2022-12-13 14:49:11 -0600

    need to go back and do day one pt2, day 4 done

---
+++ 2022/2022_04_01_pairs.py
@@ -0,0 +1,18 @@
+total = 0
+f = open("2022_04_input.txt", "r")
+for line in f:
+    print(line)
+    ranges        = line.split(',')
+    ranges[1]     = ranges[1].strip()
+    range_one     = ranges[0].split('-')
+    range_two     = ranges[1].split('-')
+    range_set_one = [ num for num in range(int(range_one[0]),int(range_one[1]) + 1,1) ]
+    range_set_two = [ num for num in range(int(range_two[0]),int(range_two[1]) + 1,1) ]
+    #print(range_set_one)
+    #print(range_set_two)
+    if set(range_set_one) & set(range_set_two) == set(range_set_one) or  set(range_set_one) & set(range_set_two) == set(range_set_two):
+        print("These overlap")
+        total = total + 1
+
+print(total)
+
